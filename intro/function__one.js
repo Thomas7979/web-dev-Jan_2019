e006fa6c90a27719911989e8c6430135a5611796
Thomas7979 <franzinformatik@bluewin.ch>
2019-01-28 11:08:13 +0100

    function__one angefügt

---
+++ intro/function__one.js
@@ -0,0 +1,9 @@
+function Fu1(myF1, myF2, myF3) {
+    var result = "";
+    result = myF1 +'  ' + myF2 +"  " + myF3;
+    return result;
+}
+
+console.log ("Test der Funktion");
+console.log (Fu1("t1", "T2xxx", "T3"));
+console.log ("ende Test");
