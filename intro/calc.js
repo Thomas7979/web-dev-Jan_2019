3465f979549e4cb7653a8a63bacda47e9164ef36
Thomas7979 <franzinformatik@bluewin.ch>
2019-01-28 07:59:52 +0100

    calculation

---
+++ intro/calc.js
@@ -0,0 +1,10 @@
+var number1 = 400
+var number2 = 1000
+var addition = number1 + number2
+var substraction = number2 - number1
+var multiplication = number1 * number2
+var division = number2 / number1
+console.log ('Add is ' + addition)
+console.log ('sub is ' + substraction)
+console.log ('muldipication is ' + multiplication)
+console.log ('div is ' + division)
