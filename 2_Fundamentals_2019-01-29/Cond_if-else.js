8b6ca847be83910272319efc334f4c6c92367d3d
Thomas7979 <franzinformatik@bluewin.ch>
2019-01-29 10:15:33 +0100

    Fundamentals

--- 2_Fundamentals_2019-01-29/Cond_if-else.js
+++ 2_Fundamentals_2019-01-29/Cond_if-else.js
@@ -6,9 +6,9 @@ if (punkte >= 40) {
     console.log('Erreichte Punkte: '+punkte)
     if (punkte >= 90) {
         console.log('Note = A')
-    } else if ( (punkte < 90) && (punkte >= 70)) {
+    } else if ( (punkte < 90) && (punkte >= 70)) {     // Bedingung mit Boolean  mit &&
         console.log('Note = B')
-    } else if (punkte >= 50) {
+    } else if (punkte >= 50) {                          // Bedingung ohne Boolean
         console.log('Note = C')
     } else if (punkte >= 40) {
         console.log('Note = D')
