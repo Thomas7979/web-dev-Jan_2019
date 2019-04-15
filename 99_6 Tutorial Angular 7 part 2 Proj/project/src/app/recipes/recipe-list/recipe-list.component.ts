2969302d01b4e0d834ab79137d49d02aca3d9365
Thomas7979 <franzinformatik@bluewin.ch>
2019-04-15 18:14:51 +0200

    HTML Course 2 basics

--- 99_6 Tutorial Angular 7 part 2 Proj/project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 99_6 Tutorial Angular 7 part 2 Proj/project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
+// link for recipe pic from internet: https://www.maxpixel.net/static/photo/1x/Recipe-Flat-Grilling-Thigh-Chicken-Barbecue-3447092.jpg
+
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe Name', 'Test Recipe Description', 'https://www.maxpixel.net/static/photo/1x/Recipe-Flat-Grilling-Thigh-Chicken-Barbecue-3447092.jpg'),  
+    new Recipe('Test Recipe Name2', 'Test Recipe Description2', '../assets/images/Misal.jpg')
+  ];
 
   constructor() { }
 
